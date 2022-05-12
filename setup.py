49008d2388662d294393b20575b97ece254fb6ab
Tristan Adams <killerdragonxs@gmail.com>
2022-05-12 14:49:25 -0400

    fy

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Reentry-AGC-Arduino',
+    version='0.2',
+    packages=[''],
+    install_requires=[
+        'pyserial'
+    ],
+    url='https://github.com/343GuiltySpark-04/Reentry-AGC-Arduino',
+    license='MIT',
+    author='343GuiltySpark-04',
+    author_email='',
+    description='Arduino Serial Interface for Reentry: An Orbital Simulator'
+)
