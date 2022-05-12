cd2c759cfb454ec44ec60246954defd174a171a7
Tristan Adams <killerdragonxs@gmail.com>
2022-05-12 10:18:36 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,5 +7,6 @@ port = serial.Serial('COM5')
 
 while True:
     data = json.load(open(sys.argv[1]))
-    port.write(str(data['IlluminateCompLight'].encode()))
-    time.sleep(10)
+    #port.write(str(data['IlluminateCompLight'].encode()))
+    port.write(str(1 if data['IlluminateCompLight'] else 0).encode())
+    time.sleep(3)
