8a52b3c1ca59332bb87c6822fc594a2c060d6c61
Tristan Adams <killerdragonxs@gmail.com>
2022-05-12 10:01:25 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
-import serial, sys, json, time
+import json
+import serial
+import sys
+import time
 
 port = serial.Serial('COM5')
 
 while True:
     data = json.load(open(sys.argv[1]))
-    port.write(str(data['IlluminateUplinkActy'].encode()))
+    port.write(str(data['IlluminateCompLight'].encode()))
     time.sleep(10)
