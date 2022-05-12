3f792a49ea0f0ec194259811ead13385b2f2555c
Tristan Adams <killerdragonxs@gmail.com>
2022-05-12 14:29:54 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,7 +9,6 @@ path_agc_json = os.path.join(path_export_apollo, 'outputAGC.json')
 port = serial.Serial('COM5')
 
 while True:
-    #data = json.load(open(sys.argv[1]))
     data = json.load(open(path_agc_json))
     port.write("#".encode())
     port.write(str(1 if data['IlluminateCompLight'] else 0).encode())
