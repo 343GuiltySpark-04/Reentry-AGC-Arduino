e97800f65b775a35d39117c1f0c56752e9c2154e
Tristan Adams <killerdragonxs@gmail.com>
2022-05-12 11:19:46 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,10 +2,14 @@ import json
 import serial
 import sys
 import time
+import os
 
+path_export_apollo = os.path.join(os.environ['APPDATA'], r'..\LocalLow\Wilhelmsen Studios\ReEntry\Export\Apollo')
+path_agc_json = os.path.join(path_export_apollo, 'outputAGC.json')
 port = serial.Serial('COM5')
 
 while True:
-    data = json.load(open(sys.argv[1]))
+    #data = json.load(open(sys.argv[1]))
+    data = json.load(open(path_agc_json))
     port.write(str(1 if data['IlluminateCompLight'] else 0).encode())
     time.sleep(3)
