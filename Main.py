5571feade32d9897004a7752a64c5bd8166cf8f8
Tristan Adams <killerdragonxs@gmail.com>
2022-05-25 13:18:02 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,65 +16,74 @@ path_export_apollo = os.path.join(os.environ['APPDATA'], r'..\LocalLow\Wilhelmse
 path_agc_json = os.path.join(path_export_apollo, 'outputAGC.json')
 port = serial.Serial('COM5')
 thread_started = False
+reg_sel = 1
 
 
 class Reader(threading.Thread):
 
-    def __init__(self):
+    def __init__(self, null):
         threading.Thread.__init__(self)
+        self.null = null
 
     def run(self):
-        reg_sel = 1
+
+        global reg_sel
         while True:
 
             if reg_sel > 3:
                 reg_sel = 1
-            port.write(str(reg_sel).encode())
             reg_sel += 1
             time.sleep(5)
 
-    while True:
-        try:
-            data = json.load(open(path_agc_json))
-            port.write("#".encode())
-            port.write(str(1 if data['IlluminateCompLight'] else 0).encode())
-            port.write(str(data['IlluminateUplinkActy']).encode())
-            port.write("<".encode())
-            port.write(str(data['Register1Sign']).encode())
-            port.write(str(data['Register1D1']).encode())
-            port.write(str(data['Register1D2']).encode())
-            port.write(str(data['Register1D3']).encode())
-            port.write(str(data['Register1D4']).encode())
-            port.write(str(data['Register1D5']).encode())
-            port.write(">".encode())
-            port.write("$".encode())
-            port.write(str(data['Register2Sign']).encode())
-            port.write(str(data['Register2D1']).encode())
-            port.write(str(data['Register2D2']).encode())
-            port.write(str(data['Register2D3']).encode())
-            port.write(str(data['Register2D4']).encode())
-            port.write(str(data['Register2D5']).encode())
-            port.write("%".encode())
-            port.write("^".encode())
-            port.write(str(data['Register3Sign']).encode())
-            port.write(str(data['Register3D1']).encode())
-            port.write(str(data['Register3D2']).encode())
-            port.write(str(data['Register3D3']).encode())
-            port.write(str(data['Register3D4']).encode())
-            port.write(str(data['Register3D5']).encode())
-            port.write("&".encode())
-            port.write("V".encode())
-            port.write(str(data['VerbD1']).encode())
-            port.write(str(data['VerbD2']).encode())
-            port.write(" ".encode())
-            port.write("N".encode())
-            port.write(str(data['NounD1']).encode())
-            port.write(str(data['NounD2']).encode())
-            port.write(" ".encode())
-            port.write("P".encode())
-            port.write(str(data['ProgramD1']).encode())
-            port.write(str(data['ProgramD2']).encode())
-            time.sleep(0.3)
-            # TODO: Spend an hour or so tinkering with the sleep delay!
-        except json.JSONDecodeError:
-            pass
+
+while True:
+    try:
+        data = json.load(open(path_agc_json))
+        t = Reader('null')
+        port.write("#".encode())
+        t.start()
+        print("#")
+        port.write(str(reg_sel).encode())
+        print(reg_sel)
+        port.write(str(1 if data['IlluminateCompLight'] else 0).encode())
+        print(str(data['IlluminateCompLight']))
+        port.write(str(data['IlluminateUplinkActy']).encode())
+        port.write("<".encode())
+        port.write(str(data['Register1Sign']).encode())
+        port.write(str(data['Register1D1']).encode())
+        port.write(str(data['Register1D2']).encode())
+        port.write(str(data['Register1D3']).encode())
+        port.write(str(data['Register1D4']).encode())
+        port.write(str(data['Register1D5']).encode())
+        port.write(">".encode())
+        port.write("$".encode())
+        port.write(str(data['Register2Sign']).encode())
+        port.write(str(data['Register2D1']).encode())
+        port.write(str(data['Register2D2']).encode())
+        port.write(str(data['Register2D3']).encode())
+        port.write(str(data['Register2D4']).encode())
+        port.write(str(data['Register2D5']).encode())
+        port.write("%".encode())
+        port.write("^".encode())
+        port.write(str(data['Register3Sign']).encode())
+        port.write(str(data['Register3D1']).encode())
+        port.write(str(data['Register3D2']).encode())
+        port.write(str(data['Register3D3']).encode())
+        port.write(str(data['Register3D4']).encode())
+        port.write(str(data['Register3D5']).encode())
+        port.write("&".encode())
+        port.write("V".encode())
+        port.write(str(data['VerbD1']).encode())
+        port.write(str(data['VerbD2']).encode())
+        port.write(" ".encode())
+        port.write("N".encode())
+        port.write(str(data['NounD1']).encode())
+        port.write(str(data['NounD2']).encode())
+        port.write(" ".encode())
+        port.write("P".encode())
+        port.write(str(data['ProgramD1']).encode())
+        port.write(str(data['ProgramD2']).encode())
+        time.sleep(0.5)
+        # TODO: Spend an hour or so tinkering with the sleep delay!
+    except json.JSONDecodeError:
+        pass
